9eda42dd1f64b31176cf7c7f230f6f23e344c72a
tokuzou0829 <77785208+tokuzou0829@users.noreply.github.com>
2023-10-06 01:21:49 +0900

    いらないスタイルの削除

--- src/app/live/[id]/page.tsx
+++ src/app/live/[id]/page.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import Image from 'next/image';
-import '../../tokuly-livestyle.css'; // スタイルシートをインポート
 import Video from './player';
 
 export default function LivePlayer({ params }: { params: { id: string } }) {
