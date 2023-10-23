b056eafda777f6437b02cd0eaa66066e19d8d6f8
tokuzou0829 <77785208+tokuzou0829@users.noreply.github.com>
2023-10-23 15:47:49 +0900

    オフラインの配信をチャンネルページに表示するように変更

--- src/app/(main)/[handle]/page.tsx
+++ src/app/(main)/[handle]/page.tsx
@@ -9,7 +9,8 @@ type Channel = {
     icon_url:string,
     self_introduction:string,
     game:string,
-    streams:Stream[]
+    streams:Stream[],
+    waiting:Stream[]
   }
   type Stream = {
     title:string,
@@ -23,6 +24,7 @@ export default async function LivePlayer({ params }: { params: { handle: string 
   body: "handle="+params.handle});
   const errorCode:Number = await res.status;
   const ch:Channel= await res.json();
+
   return (
     <div className="h-[100%] w-[100%]">
         {errorCode == 200 ?(
@@ -41,13 +43,13 @@ export default async function LivePlayer({ params }: { params: { handle: string 
                     {ch.streams.map((stream, index) => (
                         <a key={index} href={"/live/"+stream.stream_name} className="block w-[250px] shrink-0 mr-2">
                         <div className="relative">
-                            <img src={stream.thumbnail_url} className='w-[250px] rounded-lg aspect-video object-cover' />
+                            <img src={stream.thumbnail_url} className='w-[250px] rounded-lg aspect-video object-cover bg-gray-500' />
                             <div className='absolute bg-red-600 w-[100px] h-[25px] top-[5px] left-[5px] rounded-md'>
                             <p className=' text-white text-center	font-semibold	'>ライブ配信</p>
                             </div>
                         </div>
-                        <div className='flex m-1'>
-                            <img src={ch.icon_url} className='w-[40px] h-[40px] rounded-full aspect-square mr-1 object-cover'/>
+                        <div className='flex m-1 mr-0'>
+                            <img src={ch.icon_url} className='w-[40px] h-[40px] rounded-full aspect-square mr-1 object-cover bg-gray-500'/>
                             <div>
                             <p className='font-bold mb-0 truncate w-[205px]'>{stream.title}</p>
                             <p className='mt-0 text-sm'>{ch.name}</p>
@@ -57,6 +59,27 @@ export default async function LivePlayer({ params }: { params: { handle: string 
                     ))}
                     </div>
                 </div>
+                ))}
+                {(ch.waiting.length !== 0 &&(
+                    <div className="mx-[20px] mt-[20px]">
+                        <p className=" text-xl font-semibold">このクリエイターの予定</p>
+                        <div className="overflow-scroll	w-[100%] flex py-3">
+                            {ch.waiting.map((stream, index) => (
+                            <a key={index} href={"/live/"+stream.stream_name} className="block w-[250px] shrink-0 mr-2">
+                            <div className="relative">
+                                <img src={stream.thumbnail_url} className='w-[250px] rounded-lg aspect-video object-cover bg-gray-500' />
+                            </div>
+                            <div className='flex m-1 mr-0'>
+                                <img src={ch.icon_url} className='w-[40px] h-[40px] rounded-full aspect-square mr-1 object-cover bg-gray-500'/>
+                                <div>
+                                <p className='font-bold mb-0 truncate w-[205px]'>{stream.title}</p>
+                                <p className='mt-0 text-sm'>{ch.name}</p>
+                                </div>
+                            </div>
+                            </a>
+                        ))}
+                        </div>
+                    </div>
                 ))}
                 <div className="mx-[20px] mt-[20px]">
                     <p className=" text-xl font-semibold">このクリエイターについて</p>
