f830d75e5a7881094cc1aa7364b0a5ed08deae4d
tokuzou0829 <77785208+tokuzou0829@users.noreply.github.com>
2023-10-26 01:36:32 +0900

    終了表示追加

--- src/app/(embed)/(live)/emded/[id]/page.tsx
+++ src/app/(embed)/(live)/emded/[id]/page.tsx
@@ -52,7 +52,11 @@ export default function LivePage({ params }: { params: { id: string } }) {
             ):(
                 <div style={{ width: '100%',height: '100%' , background:'black',aspectRatio:'16/9',backgroundImage:'url('+live.thumbnail_url +')',backgroundSize:'cover',position:'relative'}}>
                     <div style={{position:'absolute',backgroundColor:'rgba(0,0,0,0.6)',left:0,bottom:0,margin:10,padding:10,borderRadius:10}}>
+                    {status == 'offline' ?(
                         <p className='text-white'>ストリーマーを待っています</p>
+                    ):(
+                        <p className='text-white'>この配信はすでに終了しています</p> 
+                    )}
                     </div>
                 </div>
             )}
