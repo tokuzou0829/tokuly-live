b328b355d8bfe7996715663911c65ae93a996d30
tokuzou0829 <77785208+tokuzou0829@users.noreply.github.com>
2023-10-25 23:59:49 +0900

    フルスクリーン対応

--- src/app/(main)/live/[id]/player.tsx
+++ src/app/(main)/live/[id]/player.tsx
@@ -1,40 +1,39 @@
-import React, { Component, RefObject,useEffect } from 'react';
+"use client";
+import React, { useRef, useEffect, useState } from 'react';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPlay, faPause, faVolumeHigh } from "@fortawesome/free-solid-svg-icons";
+import { faPlay, faPause, faVolumeHigh, faExpand, faCompress } from "@fortawesome/free-solid-svg-icons";
 import Hls from 'hls.js';
 
 interface VideoProps {
   id: string;
-  className?:string;
-}
-
-interface VideoState {
-  showControls: boolean;
-  isPlaying: boolean;
-  volume: number;
+  className?: string;
 }
 
 function Player(props: VideoProps) {
-  const { id ,className} = props;
-  const myRef = React.createRef<HTMLVideoElement>();
-  const overlayRef = React.createRef<HTMLDivElement>();
+  const { id, className } = props;
+  const playerRef = useRef<HTMLDivElement | null>(null); // playerRefに型を追加
+  const myRef = useRef<HTMLVideoElement | null>(null); // myRefに型を追加
+  const overlayRef = useRef<HTMLDivElement | null>(null); // overlayRefに型を追加
 
-  const [showControls, setShowControls] = React.useState(false);
-  const [isPlaying, setIsPlaying] = React.useState(true);
-  const initialVolume = localStorage.getItem('volume') || 1;
-  const [volume, setVolume] = React.useState<any>(initialVolume)
+  const [showControls, setShowControls] = useState<boolean>(false); // showControlsに型を追加
+  const [isPlaying, setIsPlaying] = useState<boolean>(true); // isPlayingに型を追加
+  const [volume, setVolume] = useState<number>();
+  const [isFullScreen, setIsFullScreen] = useState<boolean>(false);
+
   // Load volume from local storage on component mount
   useEffect(() => {
-    const savedVolume = localStorage.getItem('volume');
-    if (savedVolume !== null) {
-      setVolume(savedVolume);
-      myRef.current!.volume = parseFloat(savedVolume);
+    const initialVolume = window?.localStorage?.getItem('volume') || '1'; // initialVolumeに型を追加    
+    if (initialVolume !== null) {
+      setVolume(parseFloat(initialVolume));
+      myRef.current!.volume = parseFloat(initialVolume);
     }
   }, []);
 
   // Save volume to local storage whenever it changes
   useEffect(() => {
-    localStorage.setItem('volume', volume.toString());
+    if(volume){
+      window?.localStorage?.setItem('volume', volume.toString());
+    }
   }, [volume]);
 
   const loadVideo = () => {
@@ -102,19 +101,57 @@ function Player(props: VideoProps) {
     }
   };
 
-  React.useEffect(() => {
+const enterFullScreen = () => {
+  if (playerRef.current) {
+    if (playerRef.current.requestFullscreen) {
+      playerRef.current.requestFullscreen();
+    }
+  }
+};
+  const exitFullScreen = () => {
+    if (document.exitFullscreen) {
+      document.exitFullscreen();
+    }
+  };
+
+  // フルスクリーンモードの切り替えを監視
+  useEffect(() => {
+    const handleFullScreenChange = () => {
+      if (
+        document.fullscreenElement
+      ) {
+        setIsFullScreen(true);
+      } else {
+        setIsFullScreen(false);
+      }
+    };
+
+    document.addEventListener('fullscreenchange', handleFullScreenChange);
+    document.addEventListener('mozfullscreenchange', handleFullScreenChange);
+    document.addEventListener('webkitfullscreenchange', handleFullScreenChange);
+    document.addEventListener('msfullscreenchange', handleFullScreenChange);
+
+    return () => {
+      document.removeEventListener('fullscreenchange', handleFullScreenChange);
+      document.removeEventListener('mozfullscreenchange', handleFullScreenChange);
+      document.removeEventListener('webkitfullscreenchange', handleFullScreenChange);
+      document.removeEventListener('msfullscreenchange', handleFullScreenChange);
+    };
+  }, []);
+
+  useEffect(() => {
     loadVideo();
   }, []);
 
   return (
-    <div className={"w-full relative player" + className}>
+    <div ref={playerRef} className={"w-full relative player" + className}>
       <video
         autoPlay
         webkit-playsinline="true"
         playsInline
         ref={myRef}
         className="w-full h-full bg-black aspect-w-16 aspect-h-9"
-        style={{maxHeight:'600px'}}
+        style={ isFullScreen ? {} : { maxHeight: '600px'} }
         onMouseEnter={handleVideoHoverEnter}
         onPlay={handleVideoPlay}
         onPause={handleVideoPause}
@@ -123,10 +160,11 @@ function Player(props: VideoProps) {
         ref={overlayRef}
         className={`absolute bottom-0 left-0 ${showControls ? 'block' : 'hidden'} h-full w-full bg-black bg-opacity-50`}
         onMouseLeave={handleVideoHoverLeave}
+        onClick={toggleControls}
       >
         <div className="flex items-end h-full mt-[-10px] ml-[10px] relative">
           <div className='flex items-center'>
-            <button onClick={toggleControls} className="text-white">
+            <button onClick={(e) => {e.stopPropagation();toggleControls();}} className="text-white">
               {isPlaying ? <FontAwesomeIcon icon={faPause} /> : <FontAwesomeIcon icon={faPlay} />}
             </button>
             <div className="flex ml-3 items-center">
@@ -138,12 +176,24 @@ function Player(props: VideoProps) {
                 step="0.01"
                 value={volume}
                 onChange={handleVolumeChange}
+                onClick={(e) => e.stopPropagation()}
                 className='ml-[5px] h-2 bg-gray-200 rounded-lg appearance-none cursor-pointer bg-gray-700'
               />
             </div>
+            <div className='ml-[auto]'>
+              {isFullScreen ? (
+              <button onClick={(e) => {e.stopPropagation();exitFullScreen()}} className="absolute bottom-[0px] right-[0px] mt-[-10px] mr-[10px] text-white">
+                <FontAwesomeIcon icon={faCompress} />
+              </button>
+            ) : (
+              <button onClick={(e) => {e.stopPropagation();enterFullScreen();}} className="absolute bottom-[0px] right-[0px] mt-[-10px] mr-[10px] text-white">
+                <FontAwesomeIcon icon={faExpand} />
+              </button>
+            )}
+            </div>
           </div>
           <div className='absolute bg-red-600 w-[100px] h-[25px] top-[20px] right-[10px] rounded-md'>
-            <p className=' text-white text-center	font-semibold	'>ライブ配信</p>
+            <p className=' text-white text-center font-semibold'>ライブ配信</p>
           </div>
         </div>
       </div>
@@ -152,5 +202,3 @@ function Player(props: VideoProps) {
 }
 
 export default Player;
-
-
