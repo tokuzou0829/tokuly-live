9f0f0665b8bc8bb6b5bb7413b3e996fbf2e628be
tokuzou0829 <77785208+tokuzou0829@users.noreply.github.com>
2023-11-05 12:45:41 +0900

    バグ修正

--- src/app/(embed)/(chat)/chat/[id]/page.tsx
+++ src/app/(embed)/(chat)/chat/[id]/page.tsx
@@ -2,8 +2,7 @@ import React from 'react';
 import Image from 'next/image';
 import DefaultErrorPage from 'next/error'
 import Chat from './chat';
-import { getServerSession } from "next-auth"
-import { handler } from '../../../../api/auth/[...nextauth]/route'
+import { auth } from '../../../../api/auth/[...nextauth]/route'
 import NextAuth, { type Session } from "next-auth";
 type Live = {
     id:number,
@@ -15,7 +14,7 @@ type Live = {
     ch_handle:string,
   }
 export default async function LivePage({ params }: { params: { id: string } }) {
-  const session:Session | null = await getServerSession(handler)
+  const session:Session | null = await auth();
   const res = await fetch("https://api.tokuly.com/live/online/check",{cache: 'no-store',method: 'POST',  headers: {
     "Content-Type": "application/x-www-form-urlencoded"
   },
