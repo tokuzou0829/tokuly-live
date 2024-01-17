21a01944085c25d0c4681d4e20041a05791930b7
yutakobaayshidev <91340399+yutakobayashidev@users.noreply.github.com>
2024-01-17 22:34:14 +0900

    Add tooltip component and navigation menu

--- src/app/(main)/chList.tsx
+++ src/app/(main)/chList.tsx
@@ -1,23 +1,48 @@
 import Link from "next/link";
+import type { Channels, ListChannel } from "@/types/channel";
 
-interface ChProps {
-  icon_url: string;
-  name: string;
-  handle: string;
-  game: string;
+export function ChList({
+  channels,
+  isCollapsed,
+}: {
+  channels: Channels;
+  isCollapsed: boolean;
+}) {
+  return (
+    <div className="space-y-3 px-4">
+      {!isCollapsed && <p>おすすめチャンネル</p>}
+      <div className="space-y-4">
+        {channels.channels.map((ch, index) => (
+          <Ch isCollapsed={isCollapsed} key={index} ch={ch} />
+        ))}
+      </div>
+    </div>
+  );
 }
 
-function Ch({ ch }: { ch: ChProps }) {
+function Ch({ ch, isCollapsed }: { ch: ListChannel; isCollapsed: boolean }) {
+  if (isCollapsed) {
+    return (
+      <Link className="block" href={`/${ch.handle}`}>
+        <img
+          alt={ch.name}
+          src={ch.icon_url}
+          className="rounded-full aspect-square object-cover h-9 w-9"
+        />
+      </Link>
+    );
+  }
+
   return (
     <Link href={`/${ch.handle}`} className="flex items-start">
       <img
         alt={ch.name}
         src={ch.icon_url}
-        className="rounded-full mr-3 h-12 w-12"
+        className="rounded-full mr-3 h-9 w-9"
       />
       <div>
         <p className="font-semibold line-clamp-1">{ch.name}</p>
-        <span className="text-gray-500">{ch.game}</span>
+        <span className="text-gray-500 text-sm">{ch.game}</span>
       </div>
     </Link>
   );
--- src/app/(main)/layout-content.tsx
+++ src/app/(main)/layout-content.tsx
@@ -5,8 +5,13 @@ import {
   ResizablePanel,
   ResizablePanelGroup,
 } from "@/components/ui/resizable";
-import Ch from "./chList";
+import { ChList } from "./chList";
 import type { Channels } from "@/types/channel";
+import { useState } from "react";
+import { cn } from "@/lib/utils";
+import { Nav } from "@/app/(main)/nav";
+import { Home, Radio } from "lucide-react";
+import { TooltipProvider } from "@/components/ui/tooltip";
 
 export default function LayoutContent({
   children,
@@ -15,27 +20,67 @@ export default function LayoutContent({
   children: React.ReactNode;
   channels: Channels;
 }) {
+  const [isCollapsed, setIsCollapsed] = useState(false);
+
   return (
-    <ResizablePanelGroup direction="horizontal" className="items-stretch">
-      <ResizablePanel
-        defaultSize={15}
-        collapsedSize={4}
-        collapsible={true}
-        minSize={10}
-        maxSize={15}
-        className="bg-white sticky top-[0px] z-20 p-4"
+    <TooltipProvider delayDuration={0}>
+      <ResizablePanelGroup
+        onLayout={(sizes: number[]) => {
+          document.cookie = `react-resizable-panels:layout=${JSON.stringify(
+            sizes
+          )}`;
+        }}
+        direction="horizontal"
+        className="items-stretch"
       >
-        <div className="space-y-3 ">
-          <p>おすすめチャンネル</p>
-          <div className="space-y-4">
-            {channels.channels.map((ch, index) => (
-              <Ch key={index} ch={ch} />
-            ))}
-          </div>
-        </div>
-      </ResizablePanel>
-      <ResizableHandle withHandle />
-      <ResizablePanel defaultSize={85}>{children}</ResizablePanel>
-    </ResizablePanelGroup>
+        <ResizablePanel
+          defaultSize={15}
+          collapsedSize={4}
+          collapsible={true}
+          minSize={10}
+          maxSize={15}
+          onCollapse={() => {
+            setIsCollapsed(true);
+            document.cookie = `react-resizable-panels:collapsed=${JSON.stringify(
+              true
+            )}`;
+          }}
+          onExpand={() => {
+            setIsCollapsed(false);
+            document.cookie = `react-resizable-panels:collapsed=${JSON.stringify(
+              false
+            )}`;
+          }}
+          className={cn(
+            "bg-white sticky top-[0px] z-20",
+            isCollapsed &&
+              "min-w-[50px] transition-all duration-300 ease-in-out"
+          )}
+        >
+          <Nav
+            isCollapsed={isCollapsed}
+            links={[
+              {
+                title: "ホーム",
+                href: "/",
+                variant: "default",
+                icon: Home,
+                label: "",
+              },
+              {
+                title: "配信を開始",
+                href: "https://tokuly.com/studio",
+                variant: "ghost",
+                label: "",
+                icon: Radio,
+              },
+            ]}
+          />
+          <ChList isCollapsed={isCollapsed} channels={channels} />
+        </ResizablePanel>
+        <ResizableHandle withHandle />
+        <ResizablePanel defaultSize={85}>{children}</ResizablePanel>
+      </ResizablePanelGroup>
+    </TooltipProvider>
   );
 }
