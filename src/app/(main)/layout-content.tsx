5661b60185fb3f8e39c1c0b6173577507023ab02
tokuzou0829 <77785208+tokuzou0829@users.noreply.github.com>
2024-11-13 17:26:51 +0900

    スマートフォンで再生中にサイドバーを非表示

--- src/app/(main)/layout-content.tsx
+++ src/app/(main)/layout-content.tsx
@@ -13,6 +13,8 @@ import { Nav } from "@/app/(main)/nav";
 import { Home, Radio } from "lucide-react";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { usePathname } from 'next/navigation';
+import { is } from "date-fns/locale";
+import { set } from "date-fns";
 
 export default function LayoutContent({
   children,
@@ -24,6 +26,7 @@ export default function LayoutContent({
   const pathname = usePathname();
   const firstLocation = pathname.startsWith('/video/') || pathname.startsWith('/live/');
   const [isCollapsed, setIsCollapsed] = useState(firstLocation ? true : false);
+  const [isWatch, setIsWatch] = useState(false);
   const panelRef = useRef<any>(null);
   const [isPhone, setIsPhone] = useState(false);
 
@@ -41,6 +44,9 @@ export default function LayoutContent({
     if (isLocation) {
       panelRef.current?.collapse();
       setIsCollapsed(true);
+      setIsWatch(true);
+    }else{
+      setIsWatch(false);
     }
     if(pathname === '/' && window.innerWidth > 1150) {
       panelRef.current?.expand();
@@ -49,65 +55,71 @@ export default function LayoutContent({
   }, [pathname]);
 
   return (
-    <TooltipProvider delayDuration={0}>
-      <ResizablePanelGroup
-        onLayout={(sizes: number[]) => {
-          document.cookie = `react-resizable-panels:layout=${JSON.stringify(
-            sizes
-          )}`;
-        }}
-        direction="horizontal"
-        className="items-stretch"
-      >
-        <ResizablePanel
-          defaultSize={firstLocation? 4 : 15}
-          collapsedSize={4}
-          ref={panelRef}
-          collapsible={true}
-          minSize={!isPhone ? 15 : 4}
-          maxSize={!isPhone ? 15 : 4}
-          onCollapse={() => {
-            setIsCollapsed(true);
-            document.cookie = `react-resizable-panels:collapsed=${JSON.stringify(
-              true
+      <TooltipProvider delayDuration={0}>
+        <ResizablePanelGroup
+          onLayout={(sizes: number[]) => {
+            document.cookie = `react-resizable-panels:layout=${JSON.stringify(
+              sizes
             )}`;
           }}
-          onExpand={() => {
-            setIsCollapsed(false);
-            document.cookie = `react-resizable-panels:collapsed=${JSON.stringify(
-              false
-            )}`;
-          }}
-          className={cn(
-            "bg-white sticky top-[0px] z-20",
-            isCollapsed &&
-              "min-w-[50px] transition-all duration-300 ease-in-out"
-          )}
+          direction="horizontal"
+          className="items-stretch"
         >
-          <Nav
-            isCollapsed={isCollapsed}
-            links={[
-              {
-                title: "ホーム",
-                href: "/",
-                variant: "default",
-                icon: Home,
-                label: "",
-              },
-              {
-                title: "配信を開始",
-                href: "https://tokuly.com/studio",
-                variant: "ghost",
-                label: "",
-                icon: Radio,
-              },
-            ]}
-          />
-          <ChList isCollapsed={isCollapsed} channels={channels} />
-        </ResizablePanel>
-        <ResizableHandle withHandle={!isPhone ? true : false} />
-        <ResizablePanel defaultSize={85}>{children}</ResizablePanel>
-      </ResizablePanelGroup>
-    </TooltipProvider>
-  );
+          {!((isWatch) && isPhone) ? (
+            <>
+              <ResizablePanel
+                defaultSize={firstLocation? 4 : 15}
+                collapsedSize={4}
+                ref={panelRef}
+                collapsible={true}
+                minSize={!isPhone ? 15 : 4}
+                maxSize={!isPhone ? 15 : 4}
+                onCollapse={() => {
+                  setIsCollapsed(true);
+                  document.cookie = `react-resizable-panels:collapsed=${JSON.stringify(
+                    true
+                  )}`;
+                }}
+                onExpand={() => {
+                  setIsCollapsed(false);
+                  document.cookie = `react-resizable-panels:collapsed=${JSON.stringify(
+                    false
+                  )}`;
+                }}
+                className={cn(
+                  "bg-white sticky top-[0px] z-20",
+                  isCollapsed &&
+                    "min-w-[50px] transition-all duration-300 ease-in-out"
+                )}
+              >
+              <Nav
+                isCollapsed={isCollapsed}
+                links={[
+                  {
+                    title: "ホーム",
+                    href: "/",
+                    variant: "default",
+                    icon: Home,
+                    label: "",
+                  },
+                  {
+                    title: "配信を開始",
+                    href: "https://tokuly.com/studio",
+                    variant: "ghost",
+                    label: "",
+                    icon: Radio,
+                  },
+                ]}
+              />
+                <ChList isCollapsed={isCollapsed} channels={channels} />
+              </ResizablePanel>
+              <ResizableHandle withHandle={!isPhone ? true : false} />
+            </>
+          ):(
+            <></>
+          )}
+          <ResizablePanel defaultSize={85}>{children}</ResizablePanel>
+        </ResizablePanelGroup>
+      </TooltipProvider>
+    );
 }
