dbe3683a642e61ca685991105895a6d5dcf9e407
tokuzou0829 <77785208+tokuzou0829@users.noreply.github.com>
2025-02-23 20:12:19 +0900

    認証処理の最適化

--- src/auth.ts
+++ src/auth.ts
@@ -63,24 +63,29 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
           expires_at: account.expires_at ?? 0,
           refresh_token: account.refresh_token,
         }
-      } else if (Date.now() < token.expires_at * 1000) {
+      } else if (Date.now() > token.expires_at) {
         // Subsequent logins, but the `access_token` is still valid
+        //console.log(token.refresh_token)
         return token
       } else {
         // Subsequent logins, but the `access_token` has expired, try to refresh it
+        //console.log('refresh_token', token.refresh_token)
         if (!token.refresh_token) throw new TypeError("Missing refresh_token")
  
         try {
           const response = await fetch("https://tokuly.com/oauth/token", {
             method: "POST",
+            headers:{
+                "Content-Type": "application/x-www-form-urlencoded"
+            },
             body: new URLSearchParams({
               client_id: process.env.AUTH_TOKULY_ID!,
               client_secret: process.env.AUTH_TOKULY_SECRET!,
               grant_type: "refresh_token",
-              refresh_token: token.refresh_token!,
+              refresh_token: token.refresh_token,
             }),
           })
- 
+           
           const tokensOrError = await response.json()
  
           if (!response.ok) throw tokensOrError
@@ -96,13 +101,14 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
             Date.now() / 1000 + newTokens.expires_in
           )
           // Some providers only issue refresh tokens once, so preserve if we did not get a new one
-          if (newTokens.refresh_token)
-            token.refresh_token = newTokens.refresh_token
+          if (newTokens.refresh_token){
+            token.refresh_token = newTokens.refresh_token;
+          }
           return token
         } catch (error) {
-          console.error("Error refreshing access_token", error)
+          //console.error("Error refreshing access_token", error)
           // If we fail to refresh the token, return an error so we can handle it on the page
-          token.error = "RefreshTokenError"
+          //token.error = "RefreshTokenError"
           return token
         }
       }
